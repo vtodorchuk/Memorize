d1a702ea21687ddb971a3f26f3880a30bdcf26c6
vtodorchuk <bozodee8@gmail.com>
2024-12-23 12:25:59 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Vlady Todorchuk on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
