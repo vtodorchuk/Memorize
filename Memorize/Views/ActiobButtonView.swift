94cd030ba578162d455e5b7cdfb8bf31e9ff1267
vtodorchuk <bozodee8@gmail.com>
2024-12-24 16:14:08 +0200

    Add ability to face up card and little animation

---
+++ Memorize/Views/ActiobButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ActiobButtonView.swift
+//  Memorize
+//
+//  Created by Vlady Todorchuk on 24.12.2024.
+//
+
+import SwiftUI
+
+struct ActiobButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActiobButtonView()
+}
